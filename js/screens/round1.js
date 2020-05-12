6a90da3639656221e029b1b58faa46126db0e683
Tomi5548319 <tomi.oross@gmail.com>
2020-05-12 13:02:08 +0200

    Zmenene mapy urovni

--- js/screens/round1.js
+++ js/screens/round1.js
@@ -15,9 +15,10 @@ function round1(canvas, roundsWon1, roundsWon2) {
     var box_size = 50;
 
     // Fill the whole map
-    var i;
+    var i, j;
     for (i = 0; i < 1600 / box_size; i++)
         for (j = app.windowOffset / box_size; j < (900 - app.windowOffset) / box_size; j++) {
+
             // Empty space for players
             if ((i < 3 && j < 3 + app.windowOffset/box_size) || (i > 1600 / box_size - 4 && j > 900 / box_size - 4 - app.windowOffset/box_size))
                 continue;
@@ -29,20 +30,15 @@ function round1(canvas, roundsWon1, roundsWon2) {
             }
 
             // Right wall
-            if(i === 1600 / box_size - 4 && j > (900 - app.windowOffset) / box_size - 7){
+            if(i === 1600 / box_size - 4 && j > (900 - app.windowOffset) / box_size - 7) {
                 window.add(new Box(canvas, "wall", i * box_size, j * box_size, box_size, box_size));
                 continue;
             }
 
-            // Iron boxes
-            if(i % 3 === 1 && i > 7 && i < 1600 / box_size - 8 && j % 3 === 1 && j > 5 && j < (900 - app.windowOffset) / box_size - 3){
-                window.add(new Box(canvas, "iron", i * box_size, j * box_size, box_size, box_size));
-                continue;
-            }
+            // Middle wall
+            if(j >= 8 && j <= 9 && i > 9 && i < 1600 / box_size - 10)
+                window.add(new Box(canvas, "wall", i * box_size, j * box_size, box_size, box_size));
 
-            // Wooden boxes
-            if ((i % 3 === 1 && i > 5 && i < 1600 / box_size - 6 && j % 3 === 1) /*|| i <= 3 || i >= 1600 / box_size - 4*/)
-                window.add(new Box(canvas, "wood", i * box_size, j * box_size, box_size, box_size));
         }
 
     window.nodes = prepareTanks(canvas, window.nodes, roundsWon1, roundsWon2);
